97c128aec5437d7fb89e3ab44915e91199ec246d
itamarcasspi <itamarcasspi@gmail.com>
2025-04-01 22:26:09 +0300

    fix

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["DEPLOY_URL"],
+    origin: ["https://jamoveo-qvvw.onrender.com"],
   },
 });
 
