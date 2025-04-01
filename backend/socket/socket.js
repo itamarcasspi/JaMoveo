f2153f6329591955770c331d66d1b12609fe0262
itamarcasspi <itamarcasspi@gmail.com>
2025-04-01 22:38:09 +0300

    socket port fix

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,9 +5,11 @@ import express from "express";
 const app = express();
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: ["https://jamoveo-qvvw.onrender.com"],
+    origin: ["http://localhost:5000"],
+    methods: ["GET","POST"]
   },
 });
 
