f9e007ee24f61a5796901427b7e0e8f120ee8f86
itamarcasspi <itamarcasspi@gmail.com>
2025-04-01 21:49:09 +0300

    script updates

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 
 import connectToMongoDB from "./db/mongodb.connector.js";
 
@@ -9,6 +10,8 @@ import scraperRoutes from "./routes/scraper.routes.js"
 
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve()
+
 dotenv.config();
 
 connectToMongoDB();
@@ -16,8 +19,7 @@ connectToMongoDB();
 const PORT = process.env.PORT;
 
 
-
-
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
 app.use(express.json());
 app.use(cors({
@@ -26,6 +28,9 @@ app.use(cors({
 app.use("/api/auth",authRoutes);
 app.use("/api/search",scraperRoutes);
 
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist",index.html));
+})
 
 server.listen(PORT,() => {
     console.log(`Listening on port ${PORT}`);
